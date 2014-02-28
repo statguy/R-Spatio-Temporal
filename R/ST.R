889a99f1863ac335ed553d48648a9e5769498f9b
Jussi Jousimo <jvj@iki.fi>
2014-02-28 09:36:10 +0200

    raster stack to brick

--- R/ST.R
+++ R/ST.R
@@ -30,6 +30,6 @@ multiRasterInterpolate <- function(xyz, variables, templateRaster, transform=ide
     return(resultRaster)
   }, templateRaster=templateRaster, transform=transform, inverseTransform=inverseTransform, .parallel=F)
   
-  return(stack(rasterList))
+  return(brick(rasterList))
 }
 
